a2f9231e5f75c29b0295a365bc80732da8b211ca
Gustavo <guzanony1234@gmail.com>
2024-05-12 03:23:14 -0300

    construindo lógica e telas de login e cadastro + construindo service para login

---
+++ src/app/models/login-form.model.ts
@@ -0,0 +1,6 @@
+import { FormControl } from "@angular/forms";
+
+export class LoginForm {
+  email!: FormControl;
+  password!: FormControl;
+}
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,10 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { LoginResponse } from '../types/login-response.type';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-private service = inject(HttpClient);
+  private readonly _httpClient = inject(HttpClient);
+
+  public login(name: string, password: string): Observable<LoginResponse> {
+    return this._httpClient.post<LoginResponse>('/login', { name, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem('auth-token', value.token)
+        sessionStorage.setItem('username', value.name)
+      })
+    )
+  }
 }
