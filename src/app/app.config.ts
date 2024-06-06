f96b0c7d658cc67bb41ad3d04add46561d610bee
Richardson Maia <jr_maia@outlook.com>
2024-06-06 17:45:34 -0300

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideToastr } from 'ngx-toastr';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+<<<<<<< Updated upstream
 feature/criacao-components
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
@@ -18,12 +19,15 @@ class AppModule {}
 
 import {MatInputModule} from "@angular/material/input";
 develop
+=======
+>>>>>>> Stashed changes
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
     provideToastr(),
+<<<<<<< Updated upstream
  feature/criacao-components
     provideHttpClient(withFetch()), provideAnimationsAsync()
 =======
@@ -31,5 +35,8 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     MatInputModule,
  develop
+=======
+    provideHttpClient(withFetch()), provideAnimationsAsync()
+>>>>>>> Stashed changes
   ]
 };
