e5e833fa2961d889bcef1d6bd3ace753fa26e7e8
RonyAndersson <ronytopol1999@gmail.com>
2024-06-05 22:22:22 -0300

    Estilos da pagina Login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, NgModule} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { provideToastr } from 'ngx-toastr';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@NgModule ({
+  imports: [
+    MatSlideToggleModule,
+  ]
+})
+class AppModule {}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
     provideToastr(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()), provideAnimationsAsync()
   ]
 };
